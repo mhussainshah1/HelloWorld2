1b248c83dab8c44fde3c1b6efbf1b271edda2d5e
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-01-28 16:28:44 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        //Declare an integer variable called num and
+        //assigned it a start of 0
+        int num =0;
+
+        /*Multi line comment
+        *
+        */
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        num = keyboard.nextInt();
+
+        System.out.println(num);
+    }
+}
